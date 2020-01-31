4d8142f8b57abdfbf3309d1fa76ae2fb22348fe3
AngelaF <ADFrys@users.noreply.github.com>
2020-01-31 17:05:29 -0600

    Fix bug so restaurant name and date submit properly and begin styling form

--- src/App.js
+++ src/App.js
@@ -7,25 +7,29 @@ import uuidv4 from 'uuid/v4'
 
 function App() {
   const [review, newReview] = useState(reviews);
-  const reviewPlace = useRef();
-  const reviewRating = useRef();
-  const reviewContent = useRef();
-  const reviewAuthor = useRef();
-  const reviewPublished = useRef();
+  const reviewPlace = useRef()
+  const reviewRating = useRef()
+  const reviewContent = useRef()
+  const reviewAuthor = useRef()
+  const reviewPublished = useRef()
 
   function addReview(e) {
-  	const placeName = reviewPlace.current.value;
-  	const rating = reviewRating.current.value;
-  	const content = reviewContent.current.value;
-  	const author = reviewAuthor.current.value;
-  	const published = reviewPublished.current.value;
+  	const place = reviewPlace.current.value
+  	const rating = reviewRating.current.value
+  	const content = reviewContent.current.value
+  	const author = reviewAuthor.current.value
+  	const published_at = reviewPublished.current.value
   	e.preventDefault();
 
-  	if (placeName === '' && rating === '' && content === '' && author === '' && published === '') return 
+  	if (place === '' && rating === '' && content === '' && author === '' && published_at === '') return 
   		newReview(review=> { 
-  			return [...review, {id: uuidv4(), placeName: placeName, rating: rating, content: content, author: author, published: published}]
+  			return [...review, {id: uuidv4(), place: place, rating: rating, content: content, author: author, published_at: published_at}]
   		})
   		reviewPlace.current.value = null
+  		reviewRating.current.value = null
+  		reviewContent.current.value = null
+  		reviewAuthor.current.value = null
+  		reviewPublished.current.value = null
   }
 
   return (
@@ -37,11 +41,11 @@ function App() {
 	      </div>
 	    </div>
 	    <form>
-	      <input ref={reviewPlace} placeholder="Restaurant Name" type="text"/>
-	      <input ref={reviewRating} placeholder="Your Rating (1 to 5)" type="text"/>
-	      <input ref={reviewContent} placeholder="Write your review" type="text"/>
-	      <input ref={reviewAuthor} placeholder="Username" type="text"/>
-	      <input ref={reviewPublished} placeholder="Date" type="text"/>
+	      <label name="Restaurant">Restaurant: </label><input name="Restaurant" ref={reviewPlace} placeholder="Restaurant Name" type="text"/>
+	      <label name="Rating">Rating (1 out of 5): </label><input name="Rating" ref={reviewRating} placeholder="Your Rating (1 to 5)" type="text"/>
+	      <label name="Review">Review:</label><input name="Review" ref={reviewContent} placeholder="Write your review" type="text"/>
+	      <label name="Username">Username: </label><input name="Username" ref={reviewAuthor} placeholder="Username" type="text"/>
+	      <label name="Date">Date: </label><input name="Date" ref={reviewPublished} placeholder="Date" type="text"/>
 	      <button onClick={addReview} type="submit">Submit Review</button>
 	  </form>
   </div>
